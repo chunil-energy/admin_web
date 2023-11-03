8e93aa4ebd289c57320df2d01c42e720f2c443b9
hongc <hongcoilhouse@gmail.com>
2023-11-03 14:04:29 +0900

    add partial trip polyline

--- src/views/gps/components/TripDialog.vue
+++ src/views/gps/components/TripDialog.vue
@@ -18,58 +18,6 @@ dayjs.locale('ko')
 export default {
   name: "TripDialog",
   setup() {
-    // const timeline = [
-    //   {
-    //     id: 1,
-    //     content: 'Applied to',
-    //     target: 'Front End Developer',
-    //     href: '#',
-    //     date: 'Sep 20',
-    //     datetime: '2020-09-20',
-    //     icon: UserIcon,
-    //     iconBackground: 'bg-gray-400',
-    //   },
-    //   {
-    //     id: 2,
-    //     content: 'Advanced to phone screening by',
-    //     target: 'Bethany Blake',
-    //     href: '#',
-    //     date: 'Sep 22',
-    //     datetime: '2020-09-22',
-    //     icon: HandThumbUpIcon,
-    //     iconBackground: 'bg-blue-500',
-    //   },
-    //   {
-    //     id: 3,
-    //     content: 'Completed phone screening with',
-    //     target: 'Martha Gardner',
-    //     href: '#',
-    //     date: 'Sep 28',
-    //     datetime: '2020-09-28',
-    //     icon: CheckIcon,
-    //     iconBackground: 'bg-green-500',
-    //   },
-    //   {
-    //     id: 4,
-    //     content: 'Advanced to interview by',
-    //     target: 'Bethany Blake',
-    //     href: '#',
-    //     date: 'Sep 30',
-    //     datetime: '2020-09-30',
-    //     icon: HandThumbUpIcon,
-    //     iconBackground: 'bg-blue-500',
-    //   },
-    //   {
-    //     id: 5,
-    //     content: 'Completed interview with',
-    //     target: 'Katherine Snyder',
-    //     href: '#',
-    //     date: 'Oct 4',
-    //     datetime: '2020-10-04',
-    //     icon: CheckIcon,
-    //     iconBackground: 'bg-green-500',
-    //   },
-    // ]
     return {defaultTextInput}
   },
   components: {
@@ -94,7 +42,7 @@ export default {
       dateRange: [new Date(), new Date()]
     }
   },
-  emits: ['closeDialog', 'drawTotalPolyline', 'removePartialPolyline', 'drawPartialPolyline'],
+  emits: ['closeDialog', 'drawTotalPolyline', 'removePartialPolyline', 'drawPartialPolyline', 'setCenter'],
   methods: {
     dateParser,
     reset() {
@@ -109,12 +57,13 @@ export default {
       this.$emit('drawTotalPolyline', this.tripData.raw)
     },
     drawPartialPolyline(position) {
-      console.log('drawPartialPolyline')
       this.$emit('drawPartialPolyline', this.tripData.raw, position)
     },
     removePartialPolyline() {
-      console.log('removePartialPolyline')
       this.$emit('removePartialPolyline')
+    },
+    setCenter(position) {
+      this.$emit('setCenter', position)
     },
     closeDialog() {
       this.reset()
@@ -160,78 +109,83 @@ export default {
     </div>
     <template v-if="tripData">
       <template v-for="dateData in Object.entries(tripData.structured)" :key="dateData[0]">
-        <div class="mb-4">
-          <h3 class="text-base font-semibold leading-7 text-gray-900">
-            {{ dateData[0] }}
-          </h3>
-        </div>
-        <div class="flow-root">
-          <ul role="list" class="-mb-8">
-            <li v-for="(position, positionIndex) in dateData[1]"
-                @mouseleave="removePartialPolyline"
-                @mouseover="drawPartialPolyline(position)">
-              <div class="relative pb-8" v-if="position.is_start === true || position.is_end === true">
-                <!--                {{ position }}-->
-                <!--                {{ positionIndex }}-->
-                <template v-if="positionIndex !== 0">
+        <div class="mb-5">
+          <div class="mb-4">
+            <h3 class="text-base font-semibold leading-7 text-gray-900">
+              {{ dateData[0] }}
+            </h3>
+          </div>
+          <div class="flow-root">
+            <ul role="list" class="-mb-8">
+              <li v-for="(position, positionIndex) in dateData[1]"
+                  @mouseout="removePartialPolyline"
+                  @mouseover="drawPartialPolyline(position)">
+                <div class="relative pb-8" v-if="position.is_start === true || position.is_end === true">
+                  <!--                {{ position }}-->
+                  <!--                {{ positionIndex }}-->
+                  <template v-if="positionIndex !== 0">
                   <span v-if="position.trip_seq !== dateData[1][positionIndex-1].trip_seq"
                         class="absolute left-2.5 top-4 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true"/>
-                </template>
-                <template v-else>
-                  <span class="absolute left-2.5 top-4 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true"/>
-                </template>
-                <div class="relative flex space-x-3">
-                  <template v-if="position.is_start && position.is_end">
-                    <div>
+                  </template>
+                  <template v-else>
+                    <span class="absolute left-2.5 top-4 -ml-px h-full w-0.5 bg-gray-300" aria-hidden="true"/>
+                  </template>
+                  <div class="relative flex space-x-3 clickable" @click="setCenter(position)">
+                    <template v-if="position.is_start && position.is_end">
+                      <div>
                       <span
                           class="bg-yellow-500 h-5 w-5 rounded-full flex items-center justify-center ring-8 ring-white">
                         <PauseIcon class="h-3 w-3 text-white" aria-hidden="true"/>
                       </span>
-                    </div>
-                    <div class="flex min-w-0 flex-1 justify-between space-x-4">
+                      </div>
+                      <div class="flex min-w-0 flex-1 justify-between space-x-4">
+                        <div>
+                          <p class="text-sm text-gray-500">1분 미만 운행(단순시동)</p>
+                        </div>
+                        <div class="whitespace-nowrap text-right text-sm text-gray-500">
+                          <time :datetime="position.time">{{ position.time }}</time>
+                        </div>
+                      </div>
+                    </template>
+                    <template v-else-if="position.is_start">
                       <div>
-                        <p class="text-sm text-gray-500">1분 미만 운행(단순시동)</p>
-                      </div>
-                      <div class="whitespace-nowrap text-right text-sm text-gray-500">
-                        <time :datetime="position.time">{{ position.time }}</time>
-                      </div>
-                    </div>
-                  </template>
-                  <template v-else-if="position.is_start">
-                    <div>
                       <span
                           class="bg-green-500 h-5 w-5 rounded-full flex items-center justify-center ring-8 ring-white">
                         <PlayIcon class="h-3 w-3 text-white" aria-hidden="true"/>
                       </span>
-                    </div>
-                    <div class="flex min-w-0 flex-1 justify-between space-x-4">
+                      </div>
+                      <div class="flex min-w-0 flex-1 justify-between space-x-4">
+                        <div v-if="dateData[1].findIndex(pos => pos.trip_seq === position.trip_seq && pos.is_end) > -1">
+                          <p class="text-sm text-gray-500">운행 시작</p>
+                        </div>
+                        <div v-else>
+                          <p class="text-sm text-gray-500">주행중</p>
+                        </div>
+                        <div class="whitespace-nowrap text-right text-sm text-gray-500">
+                          <time :datetime="position.time">{{ position.time }}</time>
+                        </div>
+                      </div>
+                    </template>
+                    <template v-else-if="position.is_end">
                       <div>
-                        <p class="text-sm text-gray-500">운행 시작</p>
-                      </div>
-                      <div class="whitespace-nowrap text-right text-sm text-gray-500">
-                        <time :datetime="position.time">{{ position.time }}</time>
-                      </div>
-                    </div>
-                  </template>
-                  <template v-else-if="position.is_end">
-                    <div>
                       <span class="bg-red-500 h-5 w-5 rounded-full flex items-center justify-center ring-8 ring-white">
                         <StopIcon class="h-3 w-3 text-white" aria-hidden="true"/>
                       </span>
-                    </div>
-                    <div class="flex min-w-0 flex-1 justify-between space-x-4">
-                      <div>
-                        <p class="text-sm text-gray-500">운행 종료</p>
                       </div>
-                      <div class="whitespace-nowrap text-right text-sm text-gray-500">
-                        <time :datetime="position.time">{{ position.time }}</time>
+                      <div class="flex min-w-0 flex-1 justify-between space-x-4">
+                        <div>
+                          <p class="text-sm text-gray-500">운행 종료</p>
+                        </div>
+                        <div class="whitespace-nowrap text-right text-sm text-gray-500">
+                          <time :datetime="position.time">{{ position.time }}</time>
+                        </div>
                       </div>
-                    </div>
-                  </template>
+                    </template>
+                  </div>
                 </div>
-              </div>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </template>
     </template>
