a3239e8a005c349a176618bf1ddb48bdd0e550ff
cuhong <cuhong@ichunil.com>
2023-11-08 22:11:20 +0900

    편의기능 추가

--- src/choices/vehicle.js
+++ src/choices/vehicle.js
@@ -1,4 +1,20 @@
-const vehicleTypeChoice = {'t1': '1톤 카고', 't2.5': '2.5톤 카고', 't25_w': '25톤 워킹'}
-const vehiclePositionChoice = {'t1_sl': '1톤 수집운반', 't2.5_sl': '2.5톤 수집운반'}
+const vehicleTypeChoice = {
+    't1': '1톤 카고',
+    't2.5': '2.5톤 카고',
+    't25_w': '25톤 워킹',
+    't0.7_r': '0.7톤재활용품수거차',
+    't1.5_r': '1.5톤재활용품수거차',
+    't1.6_r': '1.6톤재활용품수거차',
+    't2.7_r': '2.7톤재활용품수거차',
+    't3.1_r': '3.1톤재활용품수거차',
+}
+const vehiclePositionChoice = {
+    't1_sl': '1톤 수집운반',
+    't2.5_sl': '2.5톤 수집운반',
+    'big_rcc': '대형생활폐기물',
+}
 
 export {vehicleTypeChoice, vehiclePositionChoice}
+
+
+
