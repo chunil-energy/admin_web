20ee81ce07d9a67c25cd41a5008930f813354e51
cuhong <cuhong@ichunil.com>
2023-11-08 15:15:11 +0900

    gps_safe

--- src/views/gps/GpsView.vue
+++ src/views/gps/GpsView.vue
@@ -7,6 +7,7 @@ import {useLayoutStore} from "@/stores/layout";
 import ContextMenu from "@/views/gps/components/ContextMenu.vue";
 import TripDialog from "@/views/gps/components/TripDialog.vue";
 import {defaultTextInput} from "@/styles";
+import {XMarkIcon} from "@heroicons/vue/24/outline";
 
 export default {
   name: 'GpsView',
@@ -58,7 +59,7 @@ export default {
     // 최초 로딩시에는 설정된 트래커가 없으므로 redundant 하다
     // this.trackerList = this.gpsSession.tracker_set
   },
-  components: {TripDialog, ContextMenu, TrackerListComponents},
+  components: {XMarkIcon, TripDialog, ContextMenu, TrackerListComponents},
   computed: {
     filteredTrackerList() {
       if (this.trackerSearchQuery) {
@@ -71,7 +72,6 @@ export default {
     }
   },
   methods: {
-    setTrackerAPI,
     loadMap() {
       const script = document.createElement('script')
       script.setAttribute('src', `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${this.ncpApiKeyId}`)
@@ -89,7 +89,12 @@ export default {
             style: naver.maps.ZoomControlStyle.SMALL,
             position: naver.maps.Position.RIGHT_BOTTOM,
           },
-          mapTypeId: naver.maps.MapTypeId.NORMAL
+          mapTypeId: naver.maps.MapTypeId.NORMAL,
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: naver.maps.MapTypeControlStyle.BUTTON,
+            position: naver.maps.Position.LEFT_TOP
+          }
         }
         this.map = new naver.maps.Map(mapDiv, mapOptions);
         this.mapBoundary = this.map.getBounds();
@@ -236,29 +241,37 @@ export default {
       }
       this.tripMarkers.forEach(marker => marker.setMap(null))
     },
-    addTracker(tracker) {
+    async addTracker(tracker) {
       let trackerIndex = this.trackerList.findIndex(existingTracker => existingTracker.id === tracker.id)
       if (trackerIndex > -1) {
         // refresh된 트래커의 경우 기존 단말기 목록에서 단말기 삭제 후 재등록
-        this.removeTracker(tracker)
+        await this.removeTracker(tracker)
       }
-      this.trackerList.push(tracker)
-      trackerIndex = this.trackerList.findIndex(existingTracker => existingTracker.id === tracker.id)
+      tracker['action'] = 'add'
+      const response = await setTrackerAPI(this.gpsSession.id, [tracker])
+      let responseTracker = response[0]
+      responseTracker['action'] = 'add'
+      this.trackerList.push(responseTracker)
+      trackerIndex = this.trackerList.findIndex(existingTracker => existingTracker.id === responseTracker.id)
       // last_position이 있는 경우 마커와 폴리라인 생성
-      if (tracker.last_position) {
-        let marker = this.createMarker(tracker)
+      if (responseTracker.last_position) {
+        let marker = this.createMarker(responseTracker)
         let listener = naver.maps.Event.addListener(marker, 'click', (event) => {
-          this.showContextMenu(event, tracker)
+          this.showContextMenu(event, responseTracker)
         })
         this.trackerList[trackerIndex]['marker'] = marker
       }
     },
-    removeTracker(tracker) {
-      console.log(tracker)
+    async removeTracker(tracker) {
+      this.closeContextMenu()
+      this.tripDialogShow = false
+      this.removeTotalPolyline()
       let trackerIndex = this.trackerList.findIndex(existingTracker => existingTracker.id === tracker.id)
       if (trackerIndex <= -1) {
         return null
       }
+      tracker['action'] = 'remove'
+      const response = await setTrackerAPI(this.gpsSession.id, [tracker])
       // let polyline = this.trackerList[trackerIndex]['polyline']
       let marker = this.trackerList[trackerIndex]['marker']
       // 마커 삭제
@@ -270,14 +283,27 @@ export default {
       }
       this.trackerList.splice(trackerIndex, 1)
     },
-    setTracker(trackerList) {
-      trackerList.forEach(trackerData => {
-        if (trackerData.action === 'add') {
-          this.addTracker(trackerData)
-        } else if (trackerData.action === 'remove') {
-          this.removeTracker(trackerData)
+    async setTracker(trackerdata) {
+      console.log('trackerdata ', trackerdata)
+        if (trackerdata.action === 'add') {
+          await this.addTracker(trackerdata)
+        } else if (trackerdata.action === 'remove') {
+          await this.removeTracker(trackerdata)
         }
-      })
+      // const promises = trackerList.map(async (trackerData) => {
+      //   if (trackerData.action === 'add') {
+      //     await this.addTracker(trackerData)
+      //   } else if (trackerData.action === 'remove') {
+      //     await this.removeTracker(trackerData)
+      //   }
+      // });
+      // trackerList.forEach(trackerData => {
+      //   if (trackerData.action === 'add') {
+      //     await this.addTracker(trackerData)
+      //   } else if (trackerData.action === 'remove') {
+      //     await this.removeTracker(trackerData)
+      //   }
+      // })
     },
     updateTracker(tracker, last_position) {
       let trackerIndex = this.trackerList.findIndex(existingTracker => existingTracker.id === tracker.id)
@@ -346,23 +372,34 @@ export default {
         <div class="w-full h-max">
           <template v-for="tracker in filteredTrackerList" v-bind:key="tracker.id">
             <div
-                class="w-full h-8 rounded-full bg-white ring-2 ring-inset ring-blue-600/90 pl-2 pr-4 flex justify-start items-center clickable mb-1"
-                @click="event => showContextMenu(event, tracker)">
-              <div class="text-sm font-semibold truncate"><span
-                  class="font-bold">{{ tracker.target.type_display + ' ' }} </span>{{ tracker.target.name }}
+                :class="[tripTracker?.id === tracker.id ? 'bg-blue-600' : 'bg-white']"
+                class="w-full h-8 rounded-full ring-2 ring-inset ring-blue-600/90 pl-2 pr-2 flex justify-start clickable mb-1 items-center justify-between">
+              <div class="text-sm font-semibold truncate" :class="{'text-white': tripTracker?.id === tracker.id}"
+                   @click="event => showContextMenu(event, tracker)">
+                <span class="font-bold">{{ tracker.target.type_display + ' ' }} </span>{{ tracker.target.name }}
+              </div>
+              <div class="flex items-center">
+                <button type="button"
+                        @click="() => {closeContextMenu(); removeTracker(tracker)}"
+                        :class="{'bg-white': tripTracker?.id !== tracker.id}"
+                        class="inline-flex rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                  <span class="sr-only">Close</span>
+                  <XMarkIcon class="h-4 w-4" :class="[tripTracker?.id === tracker.id ? 'text-white' : 'text-blue-600']"
+                             aria-hidden="true"/>
+                </button>
               </div>
             </div>
           </template>
         </div>
       </div>
       <TripDialog :show="tripDialogShow"
-                  @drawTotalPolyline="positions => drawTotalPolyline(positions)"
+                  @drawTotalPolyline="drawTotalPolyline"
                   @drawPartialPolyline="drawPartialPolyline"
                   @removePartialPolyline="removePartialPolyline"
                   @setCenter="setPositionCenter"
                   @removeTotalPolyline="removeTotalPolyline"
-                  :tracker-data="tripTracker" @closeDialog="() => {tripDialogShow = false}"/>
-      <div :class="[tripDialogShow ? 'col-span-10' : 'col-span-11']">
+                  :tracker-data="tripTracker" @closeDialog="() => {tripDialogShow = false; tripTracker=null}"/>
+      <div :class="[tripDialogShow ? 'col-span-9' : 'col-span-11']">
         <div id="mapDiv" class="w-full h-screen"></div>
       </div>
     </div>
@@ -373,8 +410,7 @@ export default {
                  @closeMenu="closeContextMenu"
                  @showTripDialog="(tracker) => {tripTracker = tracker; tripDialogShow = true; setCenter(tracker)}"
                  @removeTracker="tracker => removeTracker(tracker)"
-                 @setCenter="tracker => setCenter(tracker)"/>
-    <!--    <TripDialog :show="tripDialogShow" :tracker-data="tripTracker" @closeDialog="() => {tripDialogShow = false}"/>-->
+                 @setCenter="tracker => {setCenter(tracker); tripTracker = null; tripDialogShow = false}"/>
   </div>
   <!--  <div>-->
   <!--    -->
@@ -383,46 +419,48 @@ export default {
 
 <style scoped>
 .mapMarker {
-    /* Set the marker size here */
-    width: 2rem;
-    height: 2rem;
-    border-radius: 2rem;
-    /* Set the marker color here */
-    background: #aa3300;
+  /* Set the marker size here */
+  width: 2rem;
+  height: 2rem;
+  border-radius: 2rem;
+  /* Set the marker color here */
+  background: #aa3300;
 
-    display: inline-block;
-    border-bottom-right-radius: 0;
-    position: relative;
-    transform: rotate(45deg);
+  display: inline-block;
+  border-bottom-right-radius: 0;
+  position: relative;
+  transform: rotate(45deg);
 
-    /* optional fanciness */
-    border: 1px solid #881100;
+  /* optional fanciness */
+  border: 1px solid #881100;
 }
+
 /* inner circle (optional if you don't need the central dot) */
 .mapMarker::before {
-    content: "";
-    background: white;
-    width: 50%;
-    height: 50%;
-    border-radius: 100%;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
+  content: "";
+  background: white;
+  width: 50%;
+  height: 50%;
+  border-radius: 100%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 
-    /* optional fanciness */
-    box-shadow: 0.1rem 0.1rem 0.2rem 0.1rem rgba(0, 0, 0, 0.1);
+  /* optional fanciness */
+  box-shadow: 0.1rem 0.1rem 0.2rem 0.1rem rgba(0, 0, 0, 0.1);
 }
+
 /* shadow (optional if you don't need a shadow) */
 .mapMarker::after {
-    content: "";
-    background: rgba(128, 128, 128, 0.2);
-    width: 75%;
-    height: 75%;
-    border-radius: 100%;
-    position: absolute;
-    top: 100%;
-    left: 100%;
-    transform: translate(-50%, -50%) rotate(45deg) scaleX(0.5);
+  content: "";
+  background: rgba(128, 128, 128, 0.2);
+  width: 75%;
+  height: 75%;
+  border-radius: 100%;
+  position: absolute;
+  top: 100%;
+  left: 100%;
+  transform: translate(-50%, -50%) rotate(45deg) scaleX(0.5);
 }
 </style>
--- src/views/gps/components/ContextMenu.vue
+++ src/views/gps/components/ContextMenu.vue
@@ -1,6 +1,7 @@
 <script>
 import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
 
+
 export default {
   name: "ContextMenu",
   setup() {
@@ -34,12 +35,12 @@ export default {
       this.$emit('closeMenu')
     },
     removeTracker() {
-      this.$emit('removeTracker', this.trackerData)
       this.closeMenu()
+      this.$emit('removeTracker', this.trackerData)
     },
     showTripDialog() {
-      this.$emit('showTripDialog', this.trackerData)
       this.closeMenu()
+      this.$emit('showTripDialog', this.trackerData)
     }
   },
   computed: {
@@ -57,54 +58,26 @@ export default {
         return `${this.position[1]}px`
       }
     }
-  }
+  },
+
 }
 </script>
 
 <template>
-  <!--  <div :style="{'position': 'absolute', width: '300px', height: '300px', left: positionLeft, top: positionTop}"-->
-  <!--       class="bg-white" >-->
-
-  <div :style="{width: `${width}px`, left: positionLeft, top: positionTop}" v-if="show"
-       class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-    <div class="py-1">
-      <template v-if="trackerData.last_position">
-        <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="setCenter">현위치 보기</button>
-      </template>
-      <template v-else>
-        <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full">위치정보 없음</button>
-      </template>
-      <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="showTripDialog">경로보기</button>
-      <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="removeTracker">제거</button>
+    <div ref="menu" :style="{width: `${width}px`, left: positionLeft, top: positionTop}" v-if="show"
+         class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <div class="py-1">
+        <template v-if="trackerData.last_position">
+          <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="setCenter">현위치 보기</button>
+        </template>
+        <template v-else>
+          <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full">위치정보 없음</button>
+        </template>
+        <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="showTripDialog">경로보기</button>
+        <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="removeTracker">제거</button>
+        <button class="text-gray-700 block px-4 py-2 text-sm text-left w-full" @click="closeMenu">닫기</button>
+      </div>
     </div>
-  </div>
-
-
-  <!-- <Menu as="div" class="relative inline-block text-left" v-slot="{active}">-->
-  <!--   -->
-  <!--   {{active}}-->
-  <!--      <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">-->
-  <!--      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">-->
-  <!--        <div class="py-1">-->
-  <!--          <MenuItem v-slot="{ active }">-->
-  <!--            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Account settings</a>-->
-  <!--          </MenuItem>-->
-  <!--          <MenuItem v-slot="{ active }">-->
-  <!--            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Support</a>-->
-  <!--          </MenuItem>-->
-  <!--          <MenuItem v-slot="{ active }">-->
-  <!--            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">License</a>-->
-  <!--          </MenuItem>-->
-  <!--          <form method="POST" action="#">-->
-  <!--            <MenuItem v-slot="{ active }">-->
-  <!--              <button type="submit" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm']">Sign out</button>-->
-  <!--            </MenuItem>-->
-  <!--          </form>-->
-  <!--        </div>-->
-  <!--      </MenuItems>-->
-  <!--    </transition>-->
-  <!-- </Menu>-->
-  <!--  </div>-->
 </template>
 
 <style scoped>
